565a3bb3fde25935a18c988d2c2c5c1c0ff323ed
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-22 21:26:05 +0300

    Add outlets and actions

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,7 +8,13 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var timeLeftField: NSTextField!
+    @IBOutlet weak var eggImageView: NSImageView!
 
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +27,17 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func startButtonClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+        
+    }
+    
 }
 
