412f5115ed026ee15a5406c2c1d7786c4a33c8e7
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-23 21:29:20 +0300

    Add Preferences window, add elements to it

---
+++ EggTimer/PrefsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Sabyrzhan Tynybayev on 23.09.2022.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
