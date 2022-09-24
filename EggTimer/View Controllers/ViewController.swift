7cfa21c73f1054654f0b1282d38f90e6fa40f0a3
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-24 10:14:13 +0300

    Implement EggTimerProtocol, add textToDisplay and timeToDisplay functions

--- EggTimer/View Controllers/ViewController.swift
+++ EggTimer/View Controllers/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: NSViewController {
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var resetButton: NSButton!
     
+    var eggTimer = EggTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        eggTimer.delegate = self
     }
 
     override var representedObject: Any? {
@@ -55,3 +57,59 @@ class ViewController: NSViewController {
     
 }
 
+extension ViewController: EggTimerProtocol {
+    func timeRemainingOnTimer(_ timer: EggTimer, timeRemaining: TimeInterval) {
+        updateDisplay(for: timeRemaining)
+    }
+    
+    func timerHasFinihsed(_ timer: EggTimer) {
+        updateDisplay(for: 0)
+    }
+}
+
+extension ViewController {
+    func updateDisplay(for timerReminaing: TimeInterval) {
+        timeLeftField.stringValue = textToDisplay(for: timerReminaing)
+        eggImageView.image = imageToDisplay(for: timerReminaing)
+    }
+    
+    private func textToDisplay(for timeRemaining: TimeInterval) -> String {
+        if timeRemaining == 0 {
+            return "Done!"
+        }
+        
+        let minutesRemaining = floor(timeRemaining / 60)
+        let secondsRemaining = timeRemaining - (minutesRemaining * 60)
+        
+        let secondsDisplay = String(format: "%02d", Int(secondsRemaining))
+        let timeRemainingDisplay = "\(Int(minutesRemaining)):\(secondsDisplay))"
+        
+        return timeRemainingDisplay
+    }
+    
+    private func imageToDisplay(for timeRemaining: TimeInterval) -> NSImage? {
+        let percentageComplete = 100 - (timeRemaining / 360 * 100)
+        
+        if eggTimer.isStopped {
+            let stoppedImageName = (timeRemaining == 0) ? "100" : "stopped"
+            return NSImage(named: stoppedImageName)
+        }
+        
+        let imageName: String
+        switch percentageComplete {
+        case 0 ..< 25:
+            imageName = "0"
+        case 25 ..< 50:
+            imageName = "25"
+        case 50 ..< 75:
+            imageName = "50"
+        case 75 ..< 100:
+            imageName = "75"
+        default:
+            imageName = "100"
+            
+        }
+        
+        return NSImage(named: imageName)
+    }
+}
